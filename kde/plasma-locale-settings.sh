86556c21b42c01a5776862840f5723827fe26e5f
Pedro Lopes <1161028@isep.ipp.pt>
2019-12-02 06:43:25 +0000

    Added kde dotfiles

---
+++ kde/plasma-locale-settings.sh
@@ -0,0 +1,8 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=ja_JP.UTF-8
+export LC_NUMERIC=en_GB.UTF-8
+export LC_MONETARY=en_GB.UTF-8
+export LC_MEASUREMENT=en_GB.UTF-8
